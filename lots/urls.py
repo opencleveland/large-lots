01f1adbd2510dd2f4031ab5c0d387115b578fd95
Eric van Zanten <eric.vanzanten@gmail.com>
2014-06-11 09:17:35 -0500

    Merged in Cathy's branch so I can get the models and under the hood form submission stuff going

--- lots/urls.py
+++ lots/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'lots.views.home', name='home'),
+    url(r'^$', 'lots_client.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
